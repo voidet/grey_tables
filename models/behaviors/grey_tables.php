6ced386e58a397c78efa64518cb7cb1ce45e9bb5
AD7six <andydawson76@yahoo.co.uk>
2010-05-02 18:34:17 +0800

    reduced verbosity
    use extract so that in the code you can refer to settings by their key

--- models/behaviors/grey_tables.php
+++ models/behaviors/grey_tables.php
@@ -17,7 +17,8 @@
 		}
 
 		function beforeFind(&$Model, $queryData) {
-			if(!empty($queryData['conditions'][$Model->alias.'.'.$this->settings[$Model->alias]['password']]) && !empty($queryData['conditions'][$Model->alias.'.'.$this->settings[$Model->alias]['username']]) && (empty($queryData['conditions']['avoidRecursion']) || $queryData['conditions']['avoidRecursion'] !== true)) {
+			extract($this->settings[$Model->alias]);
+			if(!empty($queryData['conditions'][$Model->alias.'.'.$password]) && !empty($queryData['conditions'][$Model->alias.'.'.$username]) && (empty($queryData['conditions']['avoidRecursion']) || $queryData['conditions']['avoidRecursion'] !== true)) {
 				$user_id = $this->findSaltedUser($Model, $queryData['conditions']);
 				if (!empty($user_id)) {
 					unset($queryData['conditions']);
@@ -29,8 +30,9 @@
 
 		function beforeSave(&$Model) {
 			if (empty($this->id) && !empty($Model->data[$Model->alias])) {
+				extract($this->settings[$Model->alias]);
 				$data = &$Model->data[$Model->alias];
-				$data[$this->settings[$Model->alias]['password']] = $this->generateSaltedPassword($data[$this->settings[$Model->alias]['password']], $data[$this->settings[$Model->alias]['field']]);
+				$password = $this->generateSaltedPassword($data[$password], $data[$field]);
 			}
 			return parent::beforeSave(&$Model);
 		}
@@ -43,28 +45,31 @@
 
 		function findSaltedUser(&$Model, $fields = array()) {
 			if (!empty($fields)) {
+				extract($this->settings[$Model->alias]);
 
 				$db =& $Model->getDataSource();
 				$passwordField
 				$passwordExpression = $db->expression(sprintf('SHA1(CONCAT(%s, %s))',
-					$db->name($this->settings[$Model->alias]['password']),
-					$db->name($this->settings[$Model->alias]['field']),
+					$db->name($password),
+					$db->name($field),
 				));
 
 				$user_id = $Model->find('first', array(
 						'conditions' => array(
-							$Model->alias.'.'.$this->settings[$Model->alias][$this->settings[$Model->alias]['username']] => $fields[$Model->alias.'.'.$this->settings[$Model->alias][$this->settings[$Model->alias]['username']]],
-							$Model->alias . '.' . $this->settings[$Model->alias]['password'] = $passwordExpression
+							$Model->alias.'.'.$username => $fields[$Model->alias.'.'.$username],
+							$Model->alias.'.'.$password = $passwordExpression
 						),
 						'fields' => array(
-							'id'),
+							'id'
+						),
 						'recursive' => -1,
-						'avoidRecursion' => true)
+						'avoidRecursion' => true
+					)
 				);
 
 				if (!empty($user_id)) {
 					$fields[$Model->alias.'.id'] = $user_id[$Model->alias]['id'];
-					unset($fields[$Model->alias.'.'.$this->settings[$Model->alias]['password']], $fields[$Model->alias.'.'.$this->settings[$Model->alias]['username']]);
+					unset($fields[$Model->alias.'.'.$password, $fields[$Model->alias.'.'.$username]);
 				}
 			}
 			return $fields;
@@ -72,9 +77,10 @@
 
 		function hashPasswords(&$data, $alias) {
 			if (isset($data[$alias]['password'])) {
+				extract($this->settings[$Model->alias]);
 				$Model->data = $data;
-				$Model->data[$alias][$this->settings[$alias]['field']] = Security::hash(String::uuid(), null, true);
-				$Model->data[$alias][$this->settings[$Model->alias]['password']] = Security::hash($data[$alias][$this->settings[$Model->alias]['password']], null, true);
+				$Model->data[$alias][$field] = Security::hash(String::uuid(), null, true);
+				$Model->data[$alias][$password] = Security::hash($data[$alias][$password], null, true);
 				return $Model->data;
 			}
 			return $data;
